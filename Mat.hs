b7253713eef60e9dcfb29bbcf35453dc2d97436e
dagezi <dagezi@gmail.com>
2014-01-04 09:36:38 +0900

    Add dimensions for Group and rename it to MatGroup. Maybe I need dependent types..

--- Mat.hs
+++ Mat.hs
@@ -1,14 +1,17 @@
 module Mat
   where
 
-class Group g where
-  gzero :: g
+
+
+class MatGroup g where
+  dim :: g -> Int
+  gzero :: Int -> g
   gadd, gsub :: g -> g -> g
   gneg :: g -> g
   gsub a b  = gadd a (gneg b)
 
-class Group f => Field f where
-  fone :: f
+class MatGroup f => MatField f where
+  fone :: Int -> f
   fmul, fdiv :: f -> f -> f
   finv :: f -> f
   fdiv a b  = fmul a (finv b)
@@ -16,43 +19,48 @@ class Group f => Field f where
 data Gr a = Gr a
   deriving (Eq, Ord, Read, Show)
 
-instance (Num a) => Group (Gr a) where
-  gzero = Gr 0
+instance (Num a) => MatGroup (Gr a) where
+  dim g = 0
+  gzero n = Gr 0
   gadd (Gr a) (Gr b) = Gr (a + b)
   gneg (Gr a) = Gr (-a)
 
-instance (Fractional a) => Field (Gr a) where
-  fone = Gr 1
+instance (Fractional a) => MatField (Gr a) where
+  fone n = Gr 1
   fmul (Gr a) (Gr b) = Gr (a * b)
   finv (Gr a) = Gr (1 / a)
 
 data Vector f = Vec [f]
   deriving (Eq, Read, Show)
 
-instance (Group f) => Group (Vector f) where
-  gzero = Vec (repeat gzero)
+instance (MatGroup f) => MatGroup (Vector f) where
+  dim (Vec a) = length a
+  gzero n = Vec (take n $ repeat (gzero 1))
   gadd (Vec a) (Vec b) = Vec (zipWith gadd a b)
   gneg (Vec a) = Vec (map gneg a)
 
-vsmul :: (Field f) => f -> Vector f -> Vector f
+vsmul :: (MatField f) => f -> Vector f -> Vector f
 vsmul a (Vec v)  = Vec (map (fmul a) v)
 
-vimul :: (Field f) => Vector f -> Vector f -> f
-vimul (Vec v0) (Vec v1)  = foldl gadd gzero (zipWith fmul v0 v1)
-
+vimul :: (MatField f) => Vector f -> Vector f -> f
+vimul (Vec v0) (Vec v1)  = foldl gadd (gzero 1) (zipWith fmul v0 v1)
 
 data Matrix f = Mat [(Vector f)]
   deriving (Eq, Read, Show)
 
-instance (Group f) => Group (Matrix f) where
-  gzero = Mat (repeat gzero)
+instance (MatGroup f) => MatGroup (Matrix f) where
+  dim (Mat m) = length m
+  gzero n = Mat (take n $ repeat (gzero n))
   gadd (Mat m0) (Mat m1) = Mat (zipWith gadd m0 m1)
   gneg (Mat m) = Mat (map gneg m)
 
-mvmul :: (Field f) => Matrix f -> Vector f -> Vector f
+mvmul :: (MatField f) => Matrix f -> Vector f -> Vector f
 mvmul (Mat m) v = Vec (map (vimul v) m)
 
-mmul :: (Field f) => Matrix f -> Matrix f -> Matrix f
-mmul m0 m1 = gzero
+mtrans :: Matrix f -> Matrix f
+mtrans (Mat m) = Mat m
+
+mmul :: (MatField f) => Matrix f -> Matrix f -> Matrix f
+mmul (Mat m0) (Mat m1) = Mat m0
 
 
