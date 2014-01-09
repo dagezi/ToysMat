425ed29814c281edfcacc2198ab1660ad1b11c94
dagezi <dagezi@gmail.com>
2014-01-10 00:42:09 +0900

    add testcase for doctest

--- Mat.hs
+++ Mat.hs
@@ -2,6 +2,7 @@ module Mat
   where
 
 import Data.List
+import Data.Ratio
 
 class Eq g => MatGroup g where
   dim :: g -> Int
@@ -39,11 +40,23 @@ instance (MatGroup f) => MatGroup (Vector f) where
   gadd (Vec a) (Vec b) = Vec (zipWith gadd a b)
   gneg (Vec a) = Vec (map gneg a)
 
+-- |
+-- Vector's scalar multiplication
+--
+-- >>> vsmul (Gr (-1.0)) (Vec [Gr 1.0, Gr 2.0])
+-- Vec [Gr (-1.0),Gr (-2.0)]
 vsmul :: (MatField f) => f -> Vector f -> Vector f
 vsmul a (Vec v)  = Vec (map (fmul a) v)
 
-vimul :: (MatField f) => Vector f -> Vector f -> f
-vimul (Vec v0) (Vec v1)  = foldl gadd (gzero 1) (zipWith fmul v0 v1)
+-- |
+-- Vector's dot product
+--
+-- >>> vdot (Vec [Gr 1.0, Gr 2.0]) (Vec [Gr (-2.0), Gr 1.0])
+-- Gr 0.0
+-- >>> vdot (Vec [Gr (1%2), Gr (3%2)]) (Vec [Gr 2, Gr 4])
+-- Gr (7 % 1)
+vdot :: (MatField f) => Vector f -> Vector f -> f
+vdot (Vec v0) (Vec v1)  = foldl gadd (gzero 1) (zipWith fmul v0 v1)
 
 velem :: Vector f -> Int -> f
 velem (Vec v) n = v !! n
@@ -60,8 +73,13 @@ instance (MatGroup f) => MatGroup (Matrix f) where
   gadd (Mat m0) (Mat m1) = Mat (zipWith gadd m0 m1)
   gneg (Mat m) = Mat (map gneg m)
 
+-- |
+-- Matrix multiplication on Vector 
+--
+-- >>> mvmul (Mat [Vec [Gr 1, Gr 2], Vec [Gr 0, Gr 1]]) (Vec [Gr 1, Gr 2])
+-- Vec [Gr 5.0,Gr 2.0]
 mvmul :: (MatField f) => Matrix f -> Vector f -> Vector f
-mvmul (Mat m) v = Vec (map (vimul v) m)
+mvmul (Mat m) v = Vec (map (vdot v) m)
 
 mvecs :: Matrix f -> [Vector f]
 mvecs (Mat m) = m
@@ -73,8 +91,13 @@ mtrans :: Matrix f -> Matrix f
 mtrans (Mat m) = Mat [Vec [velem v ix | v <- m] | ix <- [0 .. length m - 1]]
 
 mmul :: (MatField f) => Matrix f -> Matrix f -> Matrix f
-mmul m0 m1 = Mat [Vec [vimul v0 v1 | v1 <- mvecs (mtrans m1)] | v0 <- mvecs m0]
+mmul m0 m1 = Mat [Vec [vdot v0 v1 | v1 <- mvecs (mtrans m1)] | v0 <- mvecs m0]
 
+-- |
+-- Identity matrix of size n
+--
+-- >>> (mone 2) :: (Matrix (Gr Float))
+-- Mat [Vec [Gr 1.0,Gr 0.0],Vec [Gr 0.0,Gr 1.0]]
 mone :: (MatField f) => Int -> Matrix f
 mone n = Mat [Vec [if i == j then one else zero | i<-[0..n-1]] | j<-[0..n-1]]
   where
