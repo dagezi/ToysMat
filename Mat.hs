654c2e3ecd0ac7cdc47b0eb82612eaa6c84eac90
dagezi <dagezi@gmail.com>
2014-01-19 21:06:53 +0900

    Helper functions.

--- Mat.hs
+++ Mat.hs
@@ -129,3 +129,41 @@ mdet (Mat (v0 : vs)) =
     removeAt _ [] = []
     removeAt n (v:vs) | n == 0  = vs
     removeAt n (v:vs) = v : removeAt (n - 1) vs
+
+minv :: (MatField f)=> Matrix f -> Matrix f
+minv (Mat vs) = 
+  minv' (length vs - 1) vs (mvecs (mone (length vs)))
+  where 
+   minv' ix ivs ovs =
+     if ix < 0 then (Mat ovs)
+     else
+       case findIndex (\v -> velem v ix /= gzero 1) (take ix vs) of
+         Nothing -> gzero (length ivs)  -- Non-invertible
+         Just pivotix ->
+           gzero  (length ivs)  -- Non-invertible     
+
+
+-- |
+-- Drop an element specified by index.
+--
+-- >>> dropByIndex 0 [1,2,3]
+-- [2,3]
+-- >>> dropByIndex 2 [1,2,3,4]
+-- [1,2,4]
+dropByIndex :: Int -> [a] -> [a]
+dropByIndex _ [] = []
+dropByIndex n (x:xs) 
+     | n <= 0    = xs
+     | otherwise = x : (dropByIndex (n - 1) xs)
+
+-- |
+-- Insert an element at position specified by index.
+--
+-- >>> insertAtIndex 0 0 [1,2,3]
+-- [0,1,2,3]
+-- >>> insertAtIndex 2 3 [1,2,4]
+-- [1,2,3,4]
+insertAtIndex :: Int -> a -> [a] -> [a]
+insertAtIndex _ x []  = [x]
+insertAtIndex n x xs | n <= 0  = x:xs
+insertAtIndex n x (x':xs) = x': (insertAtIndex (n - 1) x xs)
