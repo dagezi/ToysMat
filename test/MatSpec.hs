4038f2de1e4eb4c1a6c70207491fbaf57ea33153
dagezi <dagezi@gmail.com>
2014-01-27 06:56:08 +0900

    more tests

--- test/MatSpec.hs
+++ test/MatSpec.hs
@@ -11,8 +11,23 @@ zero4 = gzero 4
 one4 :: Matrix (Gr Double)
 one4 = mone 4
 
+two4 :: Matrix (Gr Double)
+two4 = msmul (Gr 2.0) one4
+
 spec :: Spec
 spec = do
+     describe "gadd for Matrix" $ do
+       it "one `gadd` one = two" $
+         (one4 `gadd` one4) `shouldBe` two4
+       it "one `gadd` zero = one" $
+         (one4 `gadd` zero4) `shouldBe` one4
+
+     describe "gsub for Matrix" $ do
+       it "one `gsub` one = zero" $
+         (one4 `gsub` one4) `shouldBe` zero4
+       it "two `gsub` one = one" $
+         (two4 `gsub` one4) `shouldBe` one4
+
      describe "mmul" $ do
        it "one `mmul` zero = zero" $
          (one4 `mmul` zero4) `shouldBe` zero4
