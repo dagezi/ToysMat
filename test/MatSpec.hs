005ef7d15f5949c87ca432093ee729624c3f3745
dagezi <dagezi@gmail.com>
2014-01-27 10:01:34 +0900

    Add QuickCheck tests.

--- test/MatSpec.hs
+++ test/MatSpec.hs
@@ -1,35 +1,58 @@
 module MatSpec where
 
 import Toys.Mat.Field
+import Toys.Mat.Vector
 import Toys.Mat.Mat
 
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck hiding ((.&.))
 
-zero4 :: Matrix (Gr Double)
-zero4 = gzero 4
+instance Arbitrary f => Arbitrary (Gr f) where
+  arbitrary = do
+    x <- arbitrary
+    return $ Gr x
 
-one4 :: Matrix (Gr Double)
-one4 = mone 4
+-- Supports only 4 dimension vector
+instance Arbitrary f => Arbitrary (Vector f) where
+  arbitrary = do
+    xs <- sequence [arbitrary, arbitrary, arbitrary, arbitrary]
+    return $ Vec xs
 
-two4 :: Matrix (Gr Double)
-two4 = msmul (Gr 2.0) one4
+vec0_4 :: Vector (Gr Rational)
+vec0_4 = gzero 4
+
+mat0_4 :: Matrix (Gr Rational)
+mat0_4 = gzero 4
+
+mat1_4 :: Matrix (Gr Rational)
+mat1_4 = mone 4
+
+mat2_4 :: Matrix (Gr Rational)
+mat2_4 = msmul (Gr 2) mat1_4
 
 spec :: Spec
 spec = do
      describe "gadd for Matrix" $ do
        it "one `gadd` one = two" $
-         (one4 `gadd` one4) `shouldBe` two4
+         (mat1_4 `gadd` mat1_4) `shouldBe` mat2_4
        it "one `gadd` zero = one" $
-         (one4 `gadd` zero4) `shouldBe` one4
+         (mat1_4 `gadd` mat0_4) `shouldBe` mat1_4
 
      describe "gsub for Matrix" $ do
        it "one `gsub` one = zero" $
-         (one4 `gsub` one4) `shouldBe` zero4
+         (mat1_4 `gsub` mat1_4) `shouldBe` mat0_4
        it "two `gsub` one = one" $
-         (two4 `gsub` one4) `shouldBe` one4
+         (mat2_4 `gsub` mat1_4) `shouldBe` mat1_4
+
+     describe "mvmul" $ do
+       prop "zero `mvmul` any vector == zero vector" $ \ v_4 ->
+         (mat0_4 `mvmul` v_4) == vec0_4
+       prop "one `mvmul` any vector == same vector" $ \v_4 ->
+         (mat1_4 `mvmul` v_4) == v_4
 
      describe "mmul" $ do
        it "one `mmul` zero = zero" $
-         (one4 `mmul` zero4) `shouldBe` zero4
+         (mat1_4 `mmul` mat0_4) `shouldBe` mat0_4
        it "one `mmul` one = one" $
-         (one4 `mmul` one4) `shouldBe` one4
+         (mat1_4 `mmul` mat1_4) `shouldBe` mat1_4
